2b6f94a92f79bfcb145294ddf1751683cec66b4a
M Pushkala <136706417+PUSHKALA123@users.noreply.github.com>
2025-02-15 16:12:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleEducation() {
+    var details = document.getElementById("edu-details");
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
